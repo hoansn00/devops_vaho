63078e91d41e80605508d58b18d92191fc6aaee4
Phạm Văn Hoan <61729888+hoansn00@users.noreply.github.com>
2022-03-07 22:23:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Tạ Trang My';
+const giftUrl = 'http://facebook.com/hoansn00';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng Chanh';
+const contentCard = 'Chúc em có một ngày 8/3 tràn ngập niềm vui và những nụ cười.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'TaTrangMy.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
