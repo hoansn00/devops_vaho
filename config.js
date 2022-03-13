c4d181377a02b8c5ec40dd6637525e87ba209cde
Phạm Văn Hoan <61729888+hoansn00@users.noreply.github.com>
2022-03-13 22:55:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,7 @@ const eventName = 'Cập Nhật Ảnh Đại Diện ';
 const titleCard = 'Avatar';
 const contentCard = 'Sinh ra vốn dĩ hiền lành\nDòng đời xô đẩy hoá thành hiền khô 🙈';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+
 const giftImage = 'TaTrangMy.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
