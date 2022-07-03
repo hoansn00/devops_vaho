97e932fab8a80ce0e3eb1525d2ec20191f6c2b7e
Phạm Văn Hoan <61729888+hoansn00@users.noreply.github.com>
2022-07-03 23:45:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const nameGirl = '';
 const giftUrl = 'http://facebook.com/hoansn00';
-const eventName = 'The flower that blooms in adversity is the rarest and most beautiful of all. 💕';
-const titleCard = 'Chanh';
-const contentCard = 'Sinh ra vốn dĩ hiền lành\nDòng đời xô đẩy hoá thành hiền khô 🙈';
-const img = 'TTM.png';
+const eventName = 'Chúc mừng sinh nhật My💕';
+const titleCard = 'Khổng Thị Hà My';
+const contentCard = 'Chúc em tuổi mới luôn xinh đẹp, gặp nhiều may mắn và thành công';
+const img = 'KTHM.png';
 
-const giftImage = 'TaTrangMy.png';
+const giftImage = 'KTHM.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
