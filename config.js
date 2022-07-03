27caadf5e82ecf189b6d3438b41c5469418407e1
Phạm Văn Hoan <61729888+hoansn00@users.noreply.github.com>
2022-07-03 23:47:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ const titleCard = 'Khổng Thị Hà My';
 const contentCard = 'Chúc em tuổi mới luôn xinh đẹp, gặp nhiều may mắn và thành công';
 const img = 'KTHM.png';
 
-const giftImage = 'KTHM.png';
+const giftImage = 'KTHMy.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
